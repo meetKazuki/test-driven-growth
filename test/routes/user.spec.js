8f8594f9ef1220ad0fde1a077f8d305b2164d8c0
Desmond <edemintech@gmail.com>
2020-10-14 12:57:11 +0100

    chore: write other GET methods (#1)

--- test/routes/user.spec.js
+++ test/routes/user.spec.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const supertest = require('supertest');
 const logger = require('../../src/config/logger');
 
-const TEST_URL = 'https://gorest.co.in/public-api';
+let TEST_URL = 'https://gorest.co.in/public-api';
 const request = supertest(TEST_URL);
 
 describe('User test suite', () => {
@@ -12,6 +12,30 @@ describe('User test suite', () => {
       .end((err, res) => {
         expect(res.body.data).to.not.be.empty;
         done();
+      });
+  });
+
+  it('should GET /users/:id', (done) => {
+    request
+      .get('/users/1')
+      .end((err, res) => {
+        expect(res.body.data.id).to.be.eq(1);
+        done(err);
+      });
+  });
+
+  it('should GET /users with query params', (done) => {
+    TEST_URL = 'users&page=5&gender=Female&status=Active';
+
+    request
+      .get(TEST_URL)
+      .end((err, res) => {
+        expect(res.body.data).to.not.be.empty;
+        res.body.data.forEach((data) => {
+          expect(data.gender).to.eq('Female');
+          expect(data.status).to.eq('Active');
+        });
+        done(err);
       });
   });
 });
